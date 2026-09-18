a42e17c5bd8d6b5ac3c2eebad26e7fb80c050f11
Darko Panic <daxpanic@yahoo.com>
2026-09-18 13:59:30 +0200

    replace Showcase with Login on / route

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Showcase />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/success" element={<Success />} />
